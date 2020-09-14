dc6f0a6414a6b3916054667205c16848656a3313
Mikael Pajari <mikael.pajari@live.se>
2020-09-14 12:37:11 +0200

    klar

--- Program.cs
+++ Program.cs
@@ -8,17 +8,42 @@ namespace zgy2bd
         {
             Console.WriteLine("skriv in ett ord: ");
             String Word = Console.ReadLine();
-
+            string rev = string.Empty;
             
+            
+            
+            for(int i = Word.Length -1; i>=0; i-- ){
                 
+                rev += Word[i];
+
+               
+
             }
-
+              Console.WriteLine(rev);
             
 
-            
+
             
 
 
         }
     }
 }
+            
+
+
+            
+
+
+
+
+            
+                
+            
+
+            
+
+            
+            
+
+
