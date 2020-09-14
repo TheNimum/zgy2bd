49ebc882f4f2c4f853c37bd2dd3ed95d49b8503e
Mikael Pajari <mikael.pajari@live.se>
2020-09-14 10:44:24 +0200

    pågående

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zgy2bd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett ord: ");
+            String Word = Console.ReadLine();
+
+            
+                
+            }
+
+            
+
+            
+            
+
+
+        }
+    }
+}
